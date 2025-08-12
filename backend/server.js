5fccd2e64bba543eee6a166c3a736953cf7889c4
bhaskar-S04 <sarenbhaskar328@gmail.com>
2025-08-12 16:44:44 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static(path.join("D:\\myWebsite\\backend", "../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
