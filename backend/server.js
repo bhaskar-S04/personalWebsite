dd4fbf1e06c341b47e93277f44af8e984e3383d0
bhaskar-S04 <sarenbhaskar328@gmail.com>
2025-08-12 16:26:30 +0530

    Initializing backend folder

---
+++ backend/server.js
@@ -0,0 +1,100 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static(path.join("D:\\myWebsite\\backend", "../public")));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("✅ Connected to MongoDB Atlas"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
+
+
+
+
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if(username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
+    res.status(200).json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ error: "Invalid credentials" });
+  }
+});
+
+
+
+
+
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to MySQL database");
+});
+
+
+
+
+
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  const query =
+    "INSERT INTO contact_messages (name, email, message) VALUES (?, ?, ?)";
+  db.query(query, [name, email, message], (err, result) => {
+    if (err) {
+      console.error("Error saving message: ", err);
+      res.status(500).json({ error: "Database error" });
+    }
+      res.status(200).json({ message: "Message received and saved!" });
+  });
+});
+
+
+app.get("/messages", (req, res)=> {
+  const query = "SELECT * FROM contact_messages ORDER BY id DESC";
+  db.query(query, (err, results)=> {
+    if(err) {
+      console.error("Error fetching messages:", err);
+      return res.status(500).json({error: "Failed to fetch messages"});
+    }
+    res.json(results);
+  });
+});
+
+
+
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
